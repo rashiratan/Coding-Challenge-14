8602bf4fa6fd406526f746021d625a5b7913749b
Rashi Ratan <rashiiratan@gmail.com>
2024-11-03 18:24:40 -0500

    Dynamically display tickets on the page

--- main.js
+++ main.js
@@ -6,14 +6,20 @@ async function retrieveUnresolvedTickets(userId)
     try {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
         if (!response.ok) {
-            throw new Error('Failed to fetch ticket data');
+            throw new Error('Failed to fetch ticket data'); //throw error if fetch fails
         }
         const tickets = await response.json();
-        if (tickets.length === 0) {
+        if (tickets.length === 0) { //check for tickets within array
             throw new Error('No tickets found.');
         }
+        tickets.forEach(ticket => { //display ticket details
+            const listItem = document.createElement('li');
+            listItem.textContent = `Ticket ID: ${ticket.id}, Customer Name: ${ticket.userId}, Issue Description: ${ticket.title}, Details: ${ticket.body}`;
+            ticketList.appendChild(listItem);
+        });
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error:', error.message); //catch error
     }
 }
 
+retrieveUnresolvedTickets(); //initialize function
